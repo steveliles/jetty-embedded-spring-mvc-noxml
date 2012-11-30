5edae81ce4b9ac52aea3745a1dc61d585613a328
Steve Liles <steveliles@gmail.com>
2012-11-30 08:25:30 +0000

    updated for Jetty 8.1.8

--- src/main/java/com/sjl/WebServer.java
+++ src/main/java/com/sjl/WebServer.java
@@ -4,9 +4,8 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-import javax.servlet.*;
-
 import org.eclipse.jetty.annotations.*;
+import org.eclipse.jetty.annotations.AnnotationParser.DiscoverableAnnotationHandler;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.server.nio.*;
@@ -83,71 +82,80 @@ public class WebServer
 			new AnnotationConfiguration() 
 			{
 				@Override
-				public void configure(WebAppContext aContext) throws Exception
-				{
-					boolean _metadataComplete = aContext.getMetaData().isMetaDataComplete();
-			        aContext.addDecorator(new AnnotationDecorator(aContext));   
-			      
-			        AnnotationParser _parser = null;
-			        if (!_metadataComplete)
-			        {
-			            if (aContext.getServletContext().getEffectiveMajorVersion() >= 3 || aContext.isConfigurationDiscovered())
-			            {
-			                _parser = createAnnotationParser();
-			                _parser.registerAnnotationHandler("javax.servlet.annotation.WebServlet", new WebServletAnnotationHandler(aContext));
-			                _parser.registerAnnotationHandler("javax.servlet.annotation.WebFilter", new WebFilterAnnotationHandler(aContext));
-			                _parser.registerAnnotationHandler("javax.servlet.annotation.WebListener", new WebListenerAnnotationHandler(aContext));
-			            }
-			        }
-			       
-			        List<ServletContainerInitializer> nonExcludedInitializers = getNonExcludedInitializers(aContext);
-			        _parser = registerServletContainerInitializerAnnotationHandlers(aContext, _parser, nonExcludedInitializers);
-			       
-			        if (_parser != null)
-			        {
-			            parse(aContext, _parser);			            
-			        }			       
+				public void configure(WebAppContext context) throws Exception {
+				       boolean metadataComplete = context.getMetaData().isMetaDataComplete();
+				       context.addDecorator(new AnnotationDecorator(context));   
+				      
+				       
+				       //Even if metadata is complete, we still need to scan for ServletContainerInitializers - if there are any
+				       AnnotationParser parser = null;
+				       if (!metadataComplete)
+				       {
+				           //If metadata isn't complete, if this is a servlet 3 webapp or isConfigDiscovered is true, we need to search for annotations
+				           if (context.getServletContext().getEffectiveMajorVersion() >= 3 || context.isConfigurationDiscovered())
+				           {
+				               _discoverableAnnotationHandlers.add(new WebServletAnnotationHandler(context));
+				               _discoverableAnnotationHandlers.add(new WebFilterAnnotationHandler(context));
+				               _discoverableAnnotationHandlers.add(new WebListenerAnnotationHandler(context));
+				           }
+				       }
+				       
+				       //Regardless of metadata, if there are any ServletContainerInitializers with @HandlesTypes, then we need to scan all the
+				       //classes so we can call their onStartup() methods correctly
+				       createServletContainerInitializerAnnotationHandlers(context, getNonExcludedInitializers(context));
+				       
+				       if (!_discoverableAnnotationHandlers.isEmpty() || _classInheritanceHandler != null || !_containerInitializerAnnotationHandlers.isEmpty())
+				       {           
+				           parser = createAnnotationParser();
+				           
+				           parse(context, parser);
+				           
+				           for (DiscoverableAnnotationHandler h:_discoverableAnnotationHandlers)
+				               context.getMetaData().addDiscoveredAnnotations(((AbstractDiscoverableAnnotationHandler)h).getAnnotationList());      
+				       }
+
 				}
 				
-				private void parse(final WebAppContext aContext, AnnotationParser aParser) throws Exception
-				{
-					clearAnnotationList(aParser.getAnnotationHandlers());
+				private void parse(final WebAppContext context, AnnotationParser parser) throws Exception
+				{					
 					List<Resource> _resources = getResources(getClass().getClassLoader());
+					
 					for (Resource _resource : _resources)
 					{
 						if (_resource == null)
 							return;
-				
-						ClassNameResolver _resolver = new ClassNameResolver()
+		            
+						parser.clearHandlers();
+		                for (DiscoverableAnnotationHandler h:_discoverableAnnotationHandlers)
+		                {
+		                    if (h instanceof AbstractDiscoverableAnnotationHandler)
+		                        ((AbstractDiscoverableAnnotationHandler)h).setResource(null); //
+		                }
+		                parser.registerHandlers(_discoverableAnnotationHandlers);
+		                parser.registerHandler(_classInheritanceHandler);
+		                parser.registerHandlers(_containerInitializerAnnotationHandlers);
+		                
+		                parser.parse(_resource, 
+		                             new ClassNameResolver()
 		                {
 		                    public boolean isExcluded (String name)
-		                    {        	
-		                        if (aContext.isSystemClass(name)) return true;                        	
-		                        if (aContext.isServerClass(name)) return false;
+		                    {
+		                        if (context.isSystemClass(name)) return true;
+		                        if (context.isServerClass(name)) return false;
 		                        return false;
 		                    }
 		
 		                    public boolean shouldOverride (String name)
 		                    {
 		                        //looking at webapp classpath, found already-parsed class of same name - did it come from system or duplicate in webapp?
-		                        if (aContext.isParentLoaderPriority())
+		                        if (context.isParentLoaderPriority())
 		                            return false;
 		                        return true;
 		                    }
-		                };
-						
-						if (_resource.isDirectory())						
-			                aParser.parse(_resource, _resolver);						
-						else
-							aParser.parse(_resource.getURI(), _resolver);
-					}
-		            
-		            //TODO - where to set the annotations discovered from WEB-INF/classes?    
-		            List<DiscoveredAnnotation> _annotations = new ArrayList<DiscoveredAnnotation>();
-		            gatherAnnotations(_annotations, aParser.getAnnotationHandlers());	                
-		            aContext.getMetaData().addDiscoveredAnnotations (_annotations);
+		                });
+		            }
 				}
-				
+
 				private List<Resource> getResources(ClassLoader aLoader) throws IOException
 				{
 					if (aLoader instanceof URLClassLoader)
